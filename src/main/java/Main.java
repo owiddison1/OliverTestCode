f225007954004e1f1fad52ba3826bec6cef46b49
Student   WIDDISON Oliver <08291@farnborough.ac.uk>
2020-11-06 10:00:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String greeting = ("Hello World");
+
+        System.out.println(greeting);
+
+        System.out.println(greeting.length());
+
+
+    }
